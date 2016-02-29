27e5e4093480bf1b47dd6c9a069cda3ecbf85da0
Aymeric Aveline <aymeric.aveline@gmail.com>
2016-02-29 14:49:24 +0000

    Base front for conceptauto version ter

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+
+
+@extends('layouts.main')
+
+@section('title', 'home')
+
+@section('content')
+
+
+<h1>Bienvenue sur un site d'escrocs</h1>
+<h2>Rubrique arnaque n°1</h2>
+
+    <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i></a>
+@endsection
+
