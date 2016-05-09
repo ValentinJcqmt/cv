62a03ecb1e111544b5616061e2193637922787f7
Youz <guillaume.valadas@gmail.com>
2016-05-09 10:21:49 +0200

    cleaning code

--- app/Http/Controllers/MailsController.php
+++ app/Http/Controllers/MailsController.php
@@ -38,17 +38,16 @@ class MailsController extends Controller {
     }
 
     private function sendDadAutoMail($request){
+
         $dad = new DadAutoReader();
         $car = $dad->show($request->car_id);
 
         $datas = array_merge($car, array_except($request->all(), ['car_id', 'email_confirmation']));
-
 
         Mail::send('emails.contact-for-car', ['datas' => $datas], function ($m) use ($request) {
             $m->from($request->email);
 
             $m->to($this->destinationMail)->subject('Demande de contact pour un véhicule');
         });
-
     }
 }
