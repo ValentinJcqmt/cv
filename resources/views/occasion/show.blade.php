b9e908a0cdbff965ded4b01d85c7f50f4dac7640
Youz <guillaume.valadas@gmail.com>
2016-07-05 11:07:01 +0200

    Fix front for used cars

--- resources/views/occasion/show.blade.php
+++ resources/views/occasion/show.blade.php
@@ -1,14 +1,6 @@
 @section('title', $datas->marque.' '.$datas->model)
 @extends('layouts.main')
-{{--{{dd($datas)}}--}}
 @section('content')
-        <?php
-        $img = null;
-        $directory = 'images/'.$datas->id;
-        $files = Storage::disk('concept-auto-public')->files($directory);
-        $img = array_first($files);
-        ?>
-
         {!! Breadcrumbs::render('show-one-used-car', $datas) !!}
         <div class="row">
                 <div class="col s12 m6">
@@ -41,7 +33,7 @@
                                 </a>
                                 <a class="btn tooltipped col m2 margin-right-5" data-position="bottom" data-delay="50"
                                    data-tooltip="Kilométrage">
-                                        {{ $datas->km }} km
+                                        {{ $datas->km }}km
                                 </a>
                                 {{--<a class="btn tooltipped col m2 margin-right-5" data-position="bottom" data-delay="50"--}}
                                 {{--data-tooltip="Transmission">--}}
@@ -84,16 +76,16 @@
 
         <div class="col m12">
                 <div class="owl-carousel">
-                        @unless($files)
-                                <img class="activator" src="{{ url('assets/providers/') }}/no-image.jpg"
-                                     alt="{{ str_slug($datas->marque.'-'.$datas->model.'-'.$datas->edition, '-') }}">
+                        @unless($datas->images)
+                                <img class="activator" src="{{ url('assets/providers/no-image.jpg') }}"
+                                     alt="{{ $datas->marque.' '.$datas->model.' '.$datas->edition }}">
                         @endunless
-                        @foreach($files as $file)
+                        @foreach($datas->images as $image)
                                 <div class="item">
                                         <a class="carousel-item">
                                                 <img class="activator"
-                                                     src="{{ asset('assets/providers/conceptauto/'.$file) }}"
-                                                     alt="{{ str_slug($datas->marque.'-'.$datas->modele.'-'.$datas->edition, '-') }}">
+                                                     src="{{ $image->urlImage }}"
+                                                     alt="{{ $datas->marque.' '.$datas->modele.' '.$datas->edition }}">
                                         </a>
                                 </div>
                         @endforeach
